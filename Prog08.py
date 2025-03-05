075bce94efb298350b9af5f4d050fd189e31170c
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-05 16:15:49 +0800

    Updated

--- Prog08.py
+++ Prog08.py
@@ -1,16 +1,13 @@
-# Ask user for 10 numbers
-print("Enter 10 numbers")
-n1 = int(input("1: "))
-n2 = int(input("2: "))
-n3 = int(input("3: "))
-n4 = int(input("4: "))
-n5 = int(input("5: "))
-n6 = int(input("6: "))
-n7 = int(input("7: "))
-n8 = int(input("8: "))
-n9 = int(input("9: "))
-n10 = int(input("10: "))
+# Collecting user input for 10 numbers
+print("Enter 10 numbers:")
+numbers = [] 
 
-# Get summation of given numbers
-total_sum = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10
-print(f"Sum = {total_sum}")
+for i in range(1, 11):
+    num = int(input(f"{i}: "))
+    numbers.append(num)
+
+# Identifying odd numbers
+print("Odd numbers:")
+for num in numbers:
+    if num % 2 != 0:  # Check if the number is odd
+        print(num)

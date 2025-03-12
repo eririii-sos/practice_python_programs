b498a43a8c5148d1b14769fbe6fa9f5a93468e11
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 01:13:58 +0800

    Added restriction for the user input

--- BATCH 3/display_all_numbers_without_repetition.py
+++ BATCH 3/display_all_numbers_without_repetition.py
@@ -1,6 +1,13 @@
 # User input
 print("Enter 10 numbers")
 for i in range(10):
-    number_input = int(input(f"Input {i+1}: "))
+    while True:
+        try:
 
+            number_input = int(input(f"Input {i+1}: "))
+            break
+        # Restriction so user only types numbers
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+            
 # Display output

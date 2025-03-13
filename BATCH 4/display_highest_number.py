634ed1f63e3eb1a3bfd556008f6bc60cd5634c82
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 01:40:55 +0800

    Initialized variable storage and created highest number tracking

--- BATCH 4/display_highest_number.py
+++ BATCH 4/display_highest_number.py
@@ -1,5 +1,12 @@
 # Initialize a variable to track the highest number
-# Get user input
-# Update the highest number every input
+highest_number = None
+
+while True:
+    try:
+        user_input = int(input("Enter a number: ")) # Get user input
+
+        if highest_number is None or user_input > highest_number: # Update the highest number every input
+            highest_number = user_input
+
 # Error handling for invalid input
 # Display highest number input

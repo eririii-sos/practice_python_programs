ec8af784710f6d96acab61b6bd1e9bd68792a55c
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 17:19:44 +0800

    Added duplicate handling for the user input

--- BATCH 3/display_all_numbers_without_repetition.py
+++ BATCH 3/display_all_numbers_without_repetition.py
@@ -1,3 +1,6 @@
+# Store input inside set to automatically handle duplicates
+all_input = set()  
+
 # User input
 print("Enter 10 numbers")
 for i in range(10):
@@ -5,9 +8,10 @@ for i in range(10):
         try:
 
             number_input = int(input(f"Input {i+1}: "))
+            all_input.add(number_input)
             break
         # Restriction so user only types numbers
         except ValueError:
             print("Invalid input! Please enter a number.")
-            
+
 # Display output

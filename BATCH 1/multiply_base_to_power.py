36c23b90104242fa06fb6a2518b688e6cd9d578d
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 19:52:32 +0800

    Improved variable name

--- BATCH 1/multiply_base_to_power.py
+++ BATCH 1/multiply_base_to_power.py
@@ -3,5 +3,7 @@ first_number = int(input("Enter First Number: "))
 second_number = int(input("Enter Second Number: " ))
 
 # Calculate
-x = first_number ** second_number
-print(f"{first_number} raised to {second_number} is {x}")
+base_times_power = first_number ** second_number
+
+# Display result
+print(f"{first_number} raised to {second_number} is {base_times_power}")

c3ef947e24410102d3e1346ff1659bcee7e08fb1
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 07:54:47 +0800

    Organized code and improved variable naming

--- BATCH 2/Prog01.py
+++ BATCH 2/Prog01.py
@@ -1,8 +1,8 @@
-# User Input
+# Get user Input
 first_number = input("Enter First number: ")
 second_number = input("Enter Second Number: ")
 
-# Get the smaller number
+# Find the smaller number
 numbers = (first_number, second_number)
-smaller = min(numbers)
-print(f"The smaller number is {smaller}")
+smaller_number = min(numbers)
+print(f"The smaller number is {smaller_number}") # Display output

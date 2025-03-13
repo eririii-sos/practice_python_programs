adf2de9cd1b1fb64826e25b3f8699ca9603e9321
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 23:04:47 +0800

    Updated tracking for lowest number and added error handling

--- BATCH 3/display_lowest_number.py
+++ BATCH 3/display_lowest_number.py
@@ -4,7 +4,13 @@ lowest_number = None
 while True:
     try: 
         user_input = int(input("Enter a number: ")) # Get user input
+        
+        # Update the lowest number every input
+        if lowest_number is None or user_input < lowest_number:
+            lowest_number = user_input
+    
+    except ValueError: # Error handling for invalid input
+        print("Invalid input! Terminating the program.")
+        break
 
-# Update the lowest number every input
-# Error handling for invalid input
 # Display lowest number input

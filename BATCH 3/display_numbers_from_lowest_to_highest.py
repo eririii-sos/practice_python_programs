1477dd5575594e4f3aff35e9f1c2f89108a9445a
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 23:25:39 +0800

    Added error handling for invalid input

--- BATCH 3/display_numbers_from_lowest_to_highest.py
+++ BATCH 3/display_numbers_from_lowest_to_highest.py
@@ -4,8 +4,11 @@ number_set = []
 while True:
     try:
         user_input = int(input("Enter a number: ")) # Get user input
-        number_set.append(user_input)
+        number_set.append(user_input) # Add new input to the list
 
-# Error handling for invalid input
+    except ValueError: # Error handling for invalid input
+        print("Invalid input! Terminating the program.")
+        break
+
 # Sort the list of numbers in ascending order
 # Display the numbers in ascending order

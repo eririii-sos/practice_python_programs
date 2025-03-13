c66a134c97f00ed4d4ed63b6ce8f99698086d269
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 21:08:29 +0800

    Organized and renamed variable name

--- BATCH 1/Prog09.py
+++ BATCH 1/Prog09.py
@@ -1,4 +1,6 @@
-# Print all even numbers from 0 to 100 using for loop
+# Top label for output
 print("All even numbers from 0-100:")
-for num in range(0, 101, 2):
-    print(num)
+
+# Print all even numbers from 0 to 100 using for loop
+for even_numbers in range(0, 101, 2):
+    print(even_numbers)

d68f4e032f04b70badd3a74aff39dcaf828f3ad9
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 07:12:00 +0800

    Initialized input storage

--- BATCH 4/display_number_with_most_number_of_duplicates.py
+++ BATCH 4/display_number_with_most_number_of_duplicates.py
@@ -1,5 +1,11 @@
 # Initialize empty list to store every input
-# Get user input
+number_set = []
+
+while True:
+    try:
+        user_input = int(input("Enter a number: ")) # Get user input
+        number_set.append(user_input)
+
 # Error handling for invalid input
 # Find number with the most duplicates
 # Display number with the most duplicates

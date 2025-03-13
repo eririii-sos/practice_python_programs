274919a911270613d1c64a7b9f82b68311ffbaa4
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 23:00:20 +0800

    Initialized a variable for tracking user input

--- BATCH 3/display_lowest_number.py
+++ BATCH 3/display_lowest_number.py
@@ -1,5 +1,10 @@
-# Get user input
-# Track the lowest number
+# Initialize a variable to track the lowest number
+lowest_number = None
+
+while True:
+    try: 
+        user_input = int(input("Enter a number: ")) # Get user input
+
 # Update the lowest number every input
 # Error handling for invalid input
 # Display lowest number input

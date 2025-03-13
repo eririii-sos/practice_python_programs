9209bd10c2aa66d9ac90c68d193f29e25dcea789
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 07:44:55 +0800

    Finished code

--- BATCH 4/display_number_with_most_number_of_duplicates.py
+++ BATCH 4/display_number_with_most_number_of_duplicates.py
@@ -16,7 +16,7 @@ if number_set:
     number_count = {user_input: number_set.count(user_input) for user_input in set(number_set)} # Counting frequencies of all numbers
     max_count = max(number_count.values()) # Find highest count of duplicates
     most_repeating_number = [user_input for user_input, count in number_count.items() if count == max_count] # Find all numbers that have highest number of duplicates
-    print(f"The number(s) with the most duplicates is/are {most_repeating_number} (appears {max_count} times).") # Display number(s) with the most duplicates
+    print(f"The number(s) with the most duplicates is/are {most_repeating_number}, appearing {max_count} times.") # Display number(s) with the most duplicates
 
 else:
     print("No valid numbers were entered.") # In case where no numbers were entered

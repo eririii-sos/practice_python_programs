12ddd735b3e4daa6ae7f374614e069d59e32f310
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 23:20:52 +0800

    Initialized storing for number inputs

--- BATCH 3/display_numbers_from_lowest_to_highest.py
+++ BATCH 3/display_numbers_from_lowest_to_highest.py
@@ -1,5 +1,11 @@
 # Initialize an empty list to store the numbers
-# Get user input
+number_set = []
+
+while True:
+    try:
+        user_input = int(input("Enter a number: ")) # Get user input
+        number_set.append(user_input)
+
 # Error handling for invalid input
 # Sort the list of numbers in ascending order
 # Display the numbers in ascending order

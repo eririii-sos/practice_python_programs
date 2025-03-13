615a4de36cdc7d4799993c13b9e4f40bc3296aff
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 07:18:33 +0800

    Added tracking for number with most duplicate

--- BATCH 4/display_number_with_most_number_of_duplicates.py
+++ BATCH 4/display_number_with_most_number_of_duplicates.py
@@ -5,7 +5,14 @@ while True:
     try:
         user_input = int(input("Enter a number: ")) # Get user input
         number_set.append(user_input)
+    
+    except ValueError: # Error handling for invalid input
+        print("Invalid input! Terminating the program.")
+        break
 
-# Error handling for invalid input
 # Find number with the most duplicates
+if number_set:
+    most_repeating_number = max(set(number_set), key=number_set.count)
+    count = number_set.count(most_repeating_number)
+
 # Display number with the most duplicates

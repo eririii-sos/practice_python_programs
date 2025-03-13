575a196738e16f43118fc6152b3613550724f784
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 22:28:25 +0800

    Organized program flow

---
+++ BATCH 3/display_and_label_repeating_and_nonrepeating_numbers.py
@@ -0,0 +1,11 @@
+# Initialize empty list to store every input
+input_numbers = []
+
+# Get user input
+while True:
+    try:
+        user_input = int(input("Enter a number: "))
+
+# Error handling for invalid input
+# Check if input is new or a duplicate
+# If not a duplicate, add the new/unique number to the list  

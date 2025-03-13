c5d813543355216e26857726755dc7d135a4c099
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 02:06:31 +0800

    Initialized variable storage for input

--- BATCH 4/display_numbers_from_highest_to_lowest.py
+++ BATCH 4/display_numbers_from_highest_to_lowest.py
@@ -1,5 +1,11 @@
 # Initialize an empty list to store the numbers
-# Get user input
+number_set = []
+
+while True:
+    try:
+        user_input = int(input("Enter a number: ")) # Get user input
+        number_set.append(user_input) # Adds new input to the list
+        
 # Error handling for invalid input
 # Sort the list of numbers in descending order
 # Display the numbers in descending order

964efce7b17727220b3af64875b393cbf350b6a7
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-13 23:16:05 +0800

    Organized program flow

---
+++ BATCH 3/display_numbers_from_lowest_to_highest.py
@@ -0,0 +1,5 @@
+# Initialize an empty list to store the numbers
+# Get user input
+# Error handling for invalid input
+# Sort the list of numbers in ascending order
+# Display the numbers in ascending order

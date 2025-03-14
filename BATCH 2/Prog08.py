381ee2a53ca783faf1a245e4a7718fc315032b66
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 08:53:19 +0800

    Made minor changes in code

--- BATCH 2/Prog08.py
+++ BATCH 2/Prog08.py
@@ -1,8 +1,8 @@
-# Print all even numbers from 0 to 100 using for loop
-print("All even numbers from 0 to 100:")
+# Print all odd numbers from 0 to 100 using while loop
+print("All odd numbers from 0 to 100:")
 number = 1 
 
-# Print odd numbers from 1 to 100
+# Print odd numbers from 0 to 100
 while number <= 100:
     print(number) 
     number += 2

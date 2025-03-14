8072416540a6542250c8d30917e2e68e99e65826
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 15:05:08 +0800

    Added division by zero error handling

--- BATCH 1/get_quotient_of_two_numbers.py
+++ BATCH 1/get_quotient_of_two_numbers.py
@@ -3,5 +3,9 @@ first_number = float(input("Enter First Number: "))
 second_number = float(input("Enter Second Number: " ))
 
 # Calculate quotient of numbers
-quotient = first_number / second_number
-print(f"The quotient of {first_number} and {second_number} is {quotient}")
+try:
+     quotient = first_number / second_number
+     print(f"The quotient of {first_number} and {second_number} is {quotient}")
+   
+except ZeroDivisionError: # Division by zero handling
+    print("Error: Division by zero is not allowed. Terminating the program.")

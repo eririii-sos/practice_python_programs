e2a708e75d5617ad64033e4167927ae04e6d0c70
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 08:18:49 +0800

    Added division by zero handling

--- BATCH 2/Prog04.py
+++ BATCH 2/Prog04.py
@@ -1,7 +1,13 @@
-# User Input
-first_number = int(input("Enter First Number: "))
-second_number = int(input("Enter Second Number: " ))
+# Get user input
+try:
+    first_number = int(input("Enter First Number: "))
+    second_number = int(input("Enter Second Number: " ))
+    
+    # Calculate quotient of numbers without the decimal point
+    try:
+        numbers_quotient = first_number // second_number
+        print(f"The quotient of {first_number} and {second_number} is {numbers_quotient}") # Display output
+    
+    except ZeroDivisionError: # Division by zero handling
+        print("Error: Division by zero is not allowed. Terminating the program.")
 
-# Calculate quotient of numbers without the decimal point
-quotient = first_number // second_number
-print(f"The quotient of {first_number} and {second_number} is {quotient}")

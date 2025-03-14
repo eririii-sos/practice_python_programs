a6b421966fbc869b502e63caa0b6964cd3a7b8a5
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 08:59:31 +0800

    Added top label

--- BATCH 2/Prog09.py
+++ BATCH 2/Prog09.py
@@ -1,4 +1,5 @@
 # Print all numbers from 0 to 100 except those ending in zero and five
+print("All numbers from 0 to 100 not ending with zero or five:")
 for num in range(101):
     if num % 10 != 0 and num % 5 != 0: 
         print(num)

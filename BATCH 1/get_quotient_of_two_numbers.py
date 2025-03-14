12c5a20f0db2f3bee18681c3c907ca38bbca934a
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 15:19:19 +0800

    Added other error handling

--- BATCH 1/get_quotient_of_two_numbers.py
+++ BATCH 1/get_quotient_of_two_numbers.py
@@ -1,11 +1,18 @@
-# User Input
-first_number = float(input("Enter First Number: "))
-second_number = float(input("Enter Second Number: " ))
-
-# Calculate quotient of numbers
+# Get user input
 try:
-     quotient = first_number / second_number
-     print(f"The quotient of {first_number} and {second_number} is {quotient}")
-   
-except ZeroDivisionError: # Division by zero handling
+     first_number = int(input("Enter First Number: "))
+     second_number = int(input("Enter Second Number: " ))
+     
+     # Calculate quotient of numbers
+     if first_number == 0 and second_number == 0:
+          print("Error: Undefined. Terminating the program.")  # Handle division by zero or undefined 0/0
+     
+     else:
+          quotient = first_number / second_number
+          print(f"The quotient of {first_number} and {second_number} is {quotient}")
+
+except ValueError:  # Handle non-numeric input
+    print("Invalid input! Please enter a valid number.")
+
+except ZeroDivisionError:  # Division by zero handling (if second number is 0)
     print("Error: Division by zero is not allowed. Terminating the program.")

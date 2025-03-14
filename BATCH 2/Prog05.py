18e937449f3d9a20ac2d38c804fcf26207841c57
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-14 08:34:26 +0800

    Added division by zero error handling

--- BATCH 2/Prog05.py
+++ BATCH 2/Prog05.py
@@ -1,7 +1,12 @@
-# User Input
-first_number = int(input("Enter First Number: "))
-second_number = int(input("Enter Second Number: " ))
+# Get user input
+try:
+    first_number = int(input("Enter First Number: "))
+    second_number = int(input("Enter Second Number: " ))
+    
+    # Get the remainder of numbers
+    try:
+        numbers_remainder = first_number % second_number
+        print(f"The remainder of {first_number} and {second_number} is {numbers_remainder}") # Display output
 
-# Calculate quotient of numbers and print remainder
-remainder = first_number % second_number
-print(f"The remainder of {first_number} and {second_number} is {remainder}")
+    except ZeroDivisionError: # Division by zero handling
+        print("Error: Division by zero is not allowed. Terminating the program.")
